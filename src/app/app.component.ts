aa38b68b1f84f95afb78ee581816763bd4f7755f
ilya <ilya.krasnov@gmail.com>
2017-06-07 14:37:35 +0300

    Testing post commit hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Changed sth to check branch dai again for post commit";
+  title = "Test post commit hook";
 }
