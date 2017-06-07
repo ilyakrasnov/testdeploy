26813b128510f294498af6721ecd9c9a011760a4
ilya <ilya.krasnov@gmail.com>
2017-06-07 13:24:03 +0300

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works and is live on aws!';
+  title = "Isn't it cool!!!";
 }
