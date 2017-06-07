4a2b3351914bbda90e9ac195349df9983406842f
ilya <ilya.krasnov@gmail.com>
2017-06-07 14:19:13 +0300

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Isn't it cool!!!";
+  title = "Changed sth to check branch";
 }
